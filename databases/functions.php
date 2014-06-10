88095e7b383bc4b9c3987d15e6701a1bfeda4be5
Technology Training <instructor@post18.stanford.edu>
2014-06-10 14:21:45 -0700

    Beginnings of creation.

--- databases/functions.php
+++ databases/functions.php
@@ -6,3 +6,35 @@ function connect () {
 		"password"
 	);
 }
+
+function recordResponse ($response, $pdo) {
+	if (!empty($response["firstName"])) {
+		$firstName = $response["firstName"];
+	} else {
+		$errors[] = "A first name is required.";
+	}
+	if (!empty($response["lastName"])) {
+		$lastName = $response["lastName"];
+	} else {
+		$errors[] = "A last name is required.";
+	}
+	if (!empty($response["phone"])) {
+		$phone = $response["phone"];
+	} else {
+		$errors[] = "A phone number is required.";
+	}
+	if (!empty($response["attending"])) {
+		$attending = $response["attending"];
+	} else {
+		$errors[] = "Will you be attending?";
+	}
+	if (isset($firstName) &&
+		isset($lastName) && 
+		isset($phone) &&
+		isset($attending)) {
+		//Database code to go here.
+		return "<p>Thank you for your rsvp.</p>";
+	} else {
+		return "<ul><li>" . implode("</li><li>", $errors) . "</li></ul>";
+	}
+}
